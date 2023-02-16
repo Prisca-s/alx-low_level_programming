b823b754d921aa79c2e054c47e77d1fc6b766f12
Prisca-s <olaprisca41@gmail.com>
2023-02-16 15:09:58 -0800

    task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *My hello, printf task
- *Return: return 1 after ptogram execution
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a pieceof art,\n");
-return(0);
-}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}}
